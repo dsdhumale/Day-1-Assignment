f651b225ca29f316f5e4d77c4a1f9e57d6582679
dsdhumale <d.sdhumale@gmail.com>
2022-09-28 00:42:01 +0530

    Added : Year is leap year or not

---
+++ LeapYearOrNot.java
@@ -0,0 +1,19 @@
+package com.Bridgelabz.Day1;
+
+import java.util.Scanner;
+
+public class LeapYearOrNot {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter year :");
+		int year = scanner.nextInt();
+		if (year % 4 == 0 && year % 100 != 0) {
+			System.out.println(year + " is leap year ");
+
+		} else if (year % 100 == 0 && year % 400 == 0) {
+			System.out.println(year + " is a leap year");
+		} else
+			System.out.println(year + " is not a leap year");
+	}
+
+}
